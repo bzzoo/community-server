dfe88bbfd714d878021f4e9b6024da87ab874837
bzzoo <fullbster@gmail.com>
2024-08-28 20:47:49 +0900

    [FEAT] API: Oauth / JWT 로그인 기능 작성

---
+++ core/core-api/src/main/java/com/app/community/support/oauth/JWTUtil.java
@@ -0,0 +1,67 @@
+package com.app.community.support.oauth;
+
+import io.jsonwebtoken.Jwts;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+
+@Component
+public class JWTUtil {
+
+    private final SecretKey secretKey;
+
+    public JWTUtil(@Value("${spring.jwt.secret}") String secret) {
+        secretKey = new SecretKeySpec(
+                secret.getBytes(StandardCharsets.UTF_8),
+                Jwts.SIG.HS256.key().build().getAlgorithm()
+        );
+    }
+
+    public String getUsername(String token) {
+        return Jwts.parser()
+                .verifyWith(secretKey)
+                .build()
+                .parseSignedClaims(token)
+                .getPayload()
+                .get("nickname", String.class);
+    }
+
+    public Long getMemberId(String token) {
+        return Jwts.parser().verifyWith(secretKey)
+                .build().parseSignedClaims(token)
+                .getPayload().get("memberId", Long.class);
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parser().verifyWith(secretKey).build().parseSignedClaims(token);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    public Boolean isExpired(String token) {
+        return Jwts.parser()
+                .verifyWith(secretKey)
+                .build()
+                .parseSignedClaims(token)
+                .getPayload()
+                .getExpiration()
+                .before(new Date());
+    }
+
+    public String createJwt(Long memberId, String nickname) {
+        return Jwts.builder()
+                .claim("nickname", nickname)
+                .claim("memberId", memberId)
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis() + 6000000L * 60))
+                .signWith(secretKey)
+                .compact();
+    }
+}
---
+++ core/core-api/src/main/java/com/app/community/support/oauth/OauthUserService.java
@@ -0,0 +1,47 @@
+package com.app.community.support.oauth;
+
+import com.app.community.domain.agg.member.Member;
+import com.app.community.domain.agg.member.MemberRegister;
+import com.app.community.domain.agg.member.MemberSocial;
+import com.app.community.domain.agg.member.MemberSocialType;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class OauthUserService extends DefaultOAuth2UserService {
+
+    private final MemberRegister memberRegister;
+
+    @Override
+    public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
+        OAuth2User oAuth2User = super.loadUser(userRequest);
+        OauthResponse response = decideProvider(userRequest, oAuth2User);
+        MemberSocial socialInfo = new MemberSocial(
+                response.getUserId(),
+                MemberSocialType.from(response.getProviderName())
+        );
+        Member member = getOrRegister(socialInfo, response);
+        return new OauthUser(new OauthPrincipal(member.getId()));
+    }
+
+    private Member getOrRegister(MemberSocial socialInfo, OauthResponse response) {
+       return memberRegister.register(socialInfo, response.getEmail(), response.getProfileImagePath());
+    }
+
+    private OauthResponse decideProvider(OAuth2UserRequest userRequest, OAuth2User oAuth2User) {
+        String registrationId = userRequest.getClientRegistration().getRegistrationId();
+        MemberSocialType memberSocialType = MemberSocialType.from(registrationId);
+        return switch (memberSocialType) {
+            case GOOGLE -> new OauthGoogleResponse(oAuth2User.getAttributes());
+            case KAKAO -> new OauthKakaoResponse(oAuth2User.getAttributes());
+            default -> null;
+        };
+    }
+}
+
+
