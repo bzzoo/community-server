068a10f9bc81bedb5943b8f41340793115f0ea8f
bzzoo <fullbster@gmail.com>
2024-09-03 19:09:53 +0900

    [FEAT] API: 웹소켓 Config & 인터셉터 작성

---
+++ core/core-api/src/main/java/com/app/community/config/WebSocketConfig.java
@@ -0,0 +1,32 @@
+package com.app.community.config;
+
+import com.app.community.support.WebSocketAuthInterceptor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@RequiredArgsConstructor
+@EnableWebSocketMessageBroker
+@Configuration
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    private final WebSocketAuthInterceptor webSocketAuthInterceptor;
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry config) {
+        config.enableSimpleBroker("/topic", "/queue");
+        config.setApplicationDestinationPrefixes("/app");
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws")
+                .setAllowedOriginPatterns("*")
+                .addInterceptors(webSocketAuthInterceptor)
+                .addInterceptors()
+                .withSockJS();
+    }
+}
